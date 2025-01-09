b09528822fb6aa5d8dc27aab0c58278497a9f2dc
Samiksha <agarwalsamiksha88@gmail.com>
2025-01-10 01:06:29 +0530

    Added the function to play the round

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function getComputedChoice(){
+let humanScore = 0, computerScore = 0;
+
+function getComputerChoice(){
     let choice = Math.random();
     if(choice >= 0 && choice < 1/3){
         return "rock";
@@ -8,5 +10,53 @@ function getComputedChoice(){
     }
     else{
         return "scissors";
+    }
+}
+
+function getHumanChoice(){
+    let choice = prompt();
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    if(humanChoice == 'rock'){
+        if(computerChoice == 'rock'){
+            console.log("Tie.");
+        }
+        else if(computerChoice == 'paper'){
+            console.log("You lose.");
+            computerChoice++;
+        }
+        else{
+            console.log("You win.");
+            humanChoice++;
+        }
+    }
+    else if(humanChoice == 'paper'){
+        if(computerChoice == 'rock'){
+            console.log("You win.");
+            humanChoice++;
+        }
+        else if(computerChoice == 'paper'){
+            console.log("Tie.");
+        }
+        else{
+            console.log("You lose.");
+            computerChoice++;
+        }
+    }
+    else{
+        if(computerChoice == 'rock'){
+            console.log("You lose.");
+            computerChoice++;
+        }
+        else if(computerChoice == 'paper'){
+            console.log("You win.");
+            humanChoice++;
+        }
+        else{
+            console.log("Tie.");
+        }
     }
 }
